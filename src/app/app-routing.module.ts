39497846ff326b82c298964445ff866731b228e8
winterguard <aliasbonfire@gmail.com>
2021-01-22 13:43:01 +0900

    create page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: '**', component: PageNotFoundComponent },
   { path: 'crisis-center', component: CrisisListComponent },
   { path: 'heroes', component: HeroListComponent }
 ];
