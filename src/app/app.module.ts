d05d86cfd4de6f09d6fda88d6f0039490a18dbf7
winterguard <aliasbonfire@gmail.com>
2021-01-25 14:28:40 +0900

    add platform-browser dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   // order of route is important
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HeroesModule,
     AppRoutingModule,
